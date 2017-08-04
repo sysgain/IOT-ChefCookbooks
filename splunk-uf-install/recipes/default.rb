29513037eca32826bbe4a14751cf6fdc941cb845
sudheermareddy <sudheerm@sysgain.com>
2017-08-04 11:05:49 +0530

    Update default.rb

--- splunk-uf-install/recipes/default.rb
+++ splunk-uf-install/recipes/default.rb
@@ -3,7 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-
+powershell_script 'chocolatey_install' do
+  guard_interpreter :powershell_script
+  code 'iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))'
+  
+end
 chocolatey_package 'splunkforwarder' do
   version '6.5.1'
   options "--params=\"RECEIVING_INDEXER=localhost:9997\""
