3fcd840bb6942b7feb05d8b0fd86e77ae0e597c0
sudheermareddy <sudheerm@sysgain.com>
2017-08-09 11:27:32 +0530

    Update default.rb

--- splunk-uf-install/recipes/default.rb
+++ splunk-uf-install/recipes/default.rb
@@ -10,5 +10,6 @@ powershell_script 'chocolatey_install' do
 end
 chocolatey_package 'splunkforwarder' do
   version '6.5.1'
-  options "--params=\"RECEIVING_INDEXER=localhost:9997\""
+  options "--params=\"RECEIVING_INDEXER=localhost:9997 WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 WINEVENTLOG_SET_ENABLE=1\""
+  #options "--params=\"RECEIVING_INDEXER=localhost:9997\""
 end
