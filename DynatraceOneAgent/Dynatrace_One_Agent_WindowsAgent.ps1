68efa1fcd1501fa34e21efcd01e2171ec877c88d
akramullak-sysgain <akramullak@sysgain.com>
2017-08-11 19:00:05 +0530

    Update Dynatrace_One_Agent_WindowsAgent.ps1

--- DynatraceOneAgent/Dynatrace_One_Agent_WindowsAgent.ps1
+++ DynatraceOneAgent/Dynatrace_One_Agent_WindowsAgent.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri https://github.com/sysgain/IOT-ChefCookbooks/blob/master/DynatraceOneAgent/Dynatrace-OneAgent-Windows-1.123.225.exe -OutFile c:/users/Dynatrace-OneAgent-Windows-1.123.225.exe
+Invoke-WebRequest -Uri https://github.com/sysgain/IOT-ChefCookbooks/blob/master/DynatraceOneAgent/Dynatrace-OneAgent-Windows-1.123.225.exe?raw=true -OutFile c:/users/Dynatrace-OneAgent-Windows-1.123.225.exe
 c:/users/Dynatrace-OneAgent-Windows-1.123.225.exe /qn
 Start-Sleep -s 60
 Restart-Service -SERVICENAME "Dynatrace OneAgent"
