f8d789bed8c71440f2fe3b0886a83b4bcc91f909
[Akramulla K M] <[akramullak@sysgain.com]>
2017-08-12 16:07:01 +0530

    dynatrace cookbook using windows package resource
    dynatrace cookbook using windows package resource

---
+++ DynatraceOneAgent/metadata.rb
@@ -0,0 +1,9 @@
+name 'DynatraceOneAgent'
+maintainer 'Piotr Mejer'
+maintainer_email 'piotr.mejer@dynatrace.com'
+license 'MIT'
+description 'Installs the Dynatrace One Agent.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+chef_version '>= 12.1'
+depends "windows"
---
+++ DynatraceOneAgent/recipes/oneagent-windows.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook:: DynatraceOneAgent
+# Recipe:: oneagent-windows
+#
+# Copyright:: 2017, Dynatrace
+
+#gem_package 'ruby-wmi'
+#chef_gem 'ruby-wmi' do
+#  action :install
+#end
+
+include_recipe "windows"
+
+download_link = node['download_link']
+
+remote_file "c:/dyna.exe" do
+  source "#{download_link }"
+  action :create
+end
+
+windows_package 'DynatraceOneAgent' do
+  source "c:/dyna.exe"
+  options '/qn'
+  installer_type :custom
+  action :install
+end
+
+service "Dynatrace OneAgent" do
+  
+   action :restart
+
+end
