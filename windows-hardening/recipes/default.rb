edc1c145d8acfb3ba3a13216d0a2f9e07d5d4c02
SrikalaRekapalli <srekapalli@sysgain.com>
2017-08-29 15:13:52 +0530

    Update default.rb

--- windows-hardening/recipes/default.rb
+++ windows-hardening/recipes/default.rb
@@ -7,12 +7,11 @@
 return unless node['platform_family'] == 'windows'
 
 include_recipe 'windows-hardening::password_policy'
-include_recipe 'windows-hardening::security_policy'
-include_recipe 'windows-hardening::user_rights'
+#include_recipe 'windows-hardening::security_policy'
+#include_recipe 'windows-hardening::user_rights'
 include_recipe 'windows-hardening::audit'
 include_recipe 'windows-hardening::ie'
-##include_recipe 'windows-hardening::rdp'
+include_recipe 'windows-hardening::rdp'
 include_recipe 'windows-hardening::access'
 include_recipe 'windows-hardening::privacy'
 include_recipe 'windows-hardening::powershell'
-
